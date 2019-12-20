0934f2f10d2320a33282cb31c2cefa675d399da3
Monica Lee <monicalee.rb@gmail.com>
2019-12-20 14:24:43 -0800

    Configure Firestore DB.

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const devConfig = {
   apiKey: process.env.REACT_APP_DEV_API_KEY,
@@ -26,6 +27,7 @@ class Firebase {
     app.initializeApp(firebaseConfig);
 
     this.auth = app.auth();
+    this.db = app.firestore();
   }
 
   // A U T H E N T I C A T I O N  A P I
@@ -34,6 +36,12 @@ class Firebase {
   doSignOut = () => this.auth.signOut();
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
   doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // U S E R S
+  users = () => this.db.collection("users");
+  user = uid => this.db.doc(`users/${uid}`);
+  // user = uid => this.db.ref(`users/${uid}`);
+  // users = () => this.db.ref('users');
 };
 
 export default Firebase;
