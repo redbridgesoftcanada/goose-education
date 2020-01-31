a8b4e0bb41086610355b4b1940ab348eaf3d3cff
Monica Lee <lee.smonica@gmail.com>
2020-01-31 12:56:24 -0800

    Update user document data in Firestore collection.

--- src/views/EditProfileForm.js
+++ src/views/EditProfileForm.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { withFirebase } from '../components/firebase';
-import { Button, Checkbox, FormControlLabel, FormHelperText, FormLabel, TextField, Typography, makeStyles } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import { Button, Checkbox, FormControlLabel, FormHelperText, FormLabel, Snackbar, TextField, Typography, makeStyles } from "@material-ui/core";
+import { withAuthorization } from '../components/session';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,51 +19,68 @@ const useStyles = makeStyles(theme => ({
 
 const profileForm = ['first_name', 'last_name', 'username', 'email', 'phone_number', 'mobile_number', 'receive_emails', 'receive_SMS', 'public_account'];
 
-const INITIAL_STATE = {
-  firstName: '',
-  lastName: '',
-  username: '',
-  email: '',
-  phoneNumber: '',
-  mobileNumber: '',
-  publicAccount: '',
-  receieveEmails: '',
-  receieveSMS: '',
-  error: null,
-};
-
-const EditProfileFormBase = ({ firebase }) => {
+const EditProfileFormBase = (props) => {
   const classes = useStyles();
-  const [ state, setState ] = useState({...INITIAL_STATE});
-  const { email, error } = state;
+  const { authUser, firebase, user } = props;
 
+  const INITIAL_STATE = {
+    ...user,
+    error: null
+  };
+
+  const [ state, setState ] = useState({...INITIAL_STATE});
+
+  const { username, email, error } = state;
   const isInvalid = email === '';
 
-  const onChange = event => setState({ ...state, [event.target.name]: event.target.value });
+  const onChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    setState({ ...state, [name]: value });
+  }
 
   const onSubmit = event => {
-      firebase.doPasswordReset(email)
-      .then(() => setState({...INITIAL_STATE}))
-      .catch(error => setState( error ));
+    const { error, ...profileForm } = state;
+
+    firebase.user(authUser.uid).update({...profileForm})
+    .then(authUser => {
+      const user = authUser.user;
+      user.updateProfile({
+        displayName: username,
+        email
+      });
+    })
+    .catch(error => setState(error));
   
-      event.preventDefault();
+    event.preventDefault();
   }
 
   return (
+    <>
+      {/* { isSaved && 
+        <Snackbar
+          open={isSaved}
+          autoHideDuration={1000}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left'}}
+          message={<Typography variant='subtitle2'>Profile Saved.</Typography>}
+        />
+      } */}
     <form className={classes.root} noValidate autoComplete="off" onSubmit={onSubmit}>
-      
-      { profileForm.map(field => {
+      { profileForm.map((field, i) => {
           let camelCaseField = field.replace(/_([a-z])/g, function (g) { return g[1].toUpperCase(); });
           let capitalizedField = field.replace(/(?:_| |\b)(\w)/g, function($1){return $1.toUpperCase().replace('_',' ');});
+          let specializedCamelCaseField = field.replace(/_/g, "");
           switch(field) {
-            case 'public_account':
-            case 'receive_emails':
             case 'receive_SMS':
+            case 'receive_emails':
+            case 'public_account':
               return (
-                <div className={classes.formField}>
+                <div key={i} className={classes.formField}>
                   <FormControlLabel
-                  control={<Checkbox checked={state[camelCaseField]} name={camelCaseField} 
-                  // onChange={(event) => dispatch({ type: 'notifications', payload: event.target })} 
+                  control={<Checkbox {...(field === 'receive_SMS' ? { defaultChecked: state[specializedCamelCaseField], name: specializedCamelCaseField } : { defaultChecked: state[camelCaseField], name: camelCaseField })}
+                  onChange={onChange} 
                   />}
                   label={capitalizedField}
                   />
@@ -71,26 +88,29 @@ const EditProfileFormBase = ({ firebase }) => {
                     { field === 'public_account' ? 'Allow others to see my information. Please allow for 0 number of days for your account settings to be changed.' : 
                     field === 'receive_emails' ? 'I would like to receive email notifications.' : 
                     'I would like to receive SMS notifications. Additional charges may apply from your service provider.' }
-                    </FormHelperText>
+                  </FormHelperText>
                 </div>
               );
             
             default:
               return (
-                <div className={classes.formField}>
+                <div key={i} className={classes.formField}>
                   <FormLabel>{capitalizedField}</FormLabel>
-                  <TextField type="text" variant="outlined" fullWidth InputLabelProps={{ shrink: true }} name={camelCaseField}/>
+                  <TextField type="text" variant="outlined" fullWidth InputLabelProps={{ shrink: true }} 
+                  name={camelCaseField}
+                  defaultValue={state[camelCaseField]}
+                  onChange={onChange}/>
                 </div>
               );
           }
       })}
 
-      <Button fullWidth disabled={isInvalid} type="submit">Submit</Button>
+      <Button variant="contained" color="secondary" disabled={isInvalid} type="submit">Submit</Button>
       {error && <Typography variant="subtitle1">{error.message}</Typography>}
     </form>
+    </>
   )
 }
 
-const EditProfileForm = withFirebase(EditProfileFormBase);
-
-export default EditProfileForm;
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(EditProfileFormBase);
