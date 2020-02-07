37b9313b1d90384ce0c089cf499b1fbf6ab813c7
Monica Lee <monicalee.rb@gmail.com>
2020-02-06 16:15:39 -0800

    Create conditional display for Filter/Reset button based on filter state.

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import { Button, withStyles } from '@material-ui/core';
-import { SearchOutlined } from '@material-ui/icons';
+import { HighlightOffOutlined, SearchOutlined } from '@material-ui/icons';
 
 const styles = theme => ({
     filterButton: {
         float: 'left',
-        color: theme.palette.primary.light,
         border: `2px solid ${theme.palette.primary.light}`,
-        paddingLeft: theme.spacing(2),
-        paddingRight: theme.spacing(2),
         marginRight: theme.spacing(1),
+        
+        "&:hover": {
+            backgroundColor: theme.palette.primary.light
+        }
     },
 });
 
 function Filter(props) {
-    const { classes, handleFilterClick } = props;
+    const { classes, isFilter, handleFilterClick, handleFilterReset } = props;
 
     return (
-        <>
-            <Button 
-                className={classes.filterButton} 
-                onClick={handleFilterClick}
-                startIcon={<SearchOutlined/>}
-            >
-                Filter
-            </Button>
-        </>
+        <Button
+            color='inherit'
+            variant='contained'
+            size='medium' 
+            className={classes.filterButton} 
+            {...(isFilter) ? {onClick: handleFilterReset, startIcon:<HighlightOffOutlined/>} : {onClick: handleFilterClick, startIcon:<SearchOutlined/>}}
+        >
+            {(isFilter) ? 'Clear' : 'Filter' }
+        </Button>
     )
 }
 
