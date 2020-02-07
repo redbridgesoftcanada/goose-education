e663546a92fa772e20e668dd852f7c848758770d
Monica Lee <monicalee.rb@gmail.com>
2020-02-07 11:45:24 -0800

    Replace CSS with MUI props to be consistent with Filter and Sort.

--- src/components/ComposeButton.js
+++ src/components/ComposeButton.js
@@ -5,11 +5,13 @@ import CreateIcon from '@material-ui/icons/Create';
 const styles = theme => ({
     composeButton: {
         float: 'left',
-        color: theme.palette.secondary.main,
         border: `2px solid ${theme.palette.secondary.main}`,
-        paddingLeft: theme.spacing(2),
-        paddingRight: theme.spacing(2),
         marginRight: theme.spacing(1),
+
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.main,
+            color: '#FFF'
+        }
     },
 });
 
@@ -17,15 +19,16 @@ function Compose(props) {
     const { classes, handleComposeClick } = props;
 
     return (
-        <>
-            <Button 
-                className={classes.composeButton} 
-                onClick={handleComposeClick}
-                startIcon={<CreateIcon/>}
-            >
-                Compose
-            </Button>
-        </>
+        <Button
+            variant='outlined'
+            color='secondary'
+            size='medium'
+            className={classes.composeButton} 
+            onClick={handleComposeClick}
+            startIcon={<CreateIcon/>}
+        >
+            Compose
+        </Button>
     )
 }
 
