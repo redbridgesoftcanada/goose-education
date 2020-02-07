df08024c552a95fbb36fe88d844c63d24e334b9a
Monica Lee <monicalee.rb@gmail.com>
2020-02-07 09:44:37 -0800

    Create helper functions for filter (single, multiple) and sort.

---
+++ src/constants/helpers.js
@@ -0,0 +1,121 @@
+export function singleFilterQuery(resources, option, words) {
+    const filterWords = words.toLowerCase();
+    let filteredContent = [];
+    switch(option) {
+        case 'Title':
+            filteredContent = resources.filter(resource => {
+                const resourceTitle = resource.title.toLowerCase();
+                return resourceTitle.includes(filterWords);
+            });
+            break;
+
+        case 'Contents':
+            filteredContent = resources.filter(resource => {
+                const resourceContent = resource.description.toLowerCase();
+                return resourceContent.includes(filterWords);
+            });
+            break;
+
+        case 'Title + Contents':
+            filteredContent = resources.filter(resource => {
+                const resourceTitle = resource.title.toLowerCase();
+                const resourceContent = resource.description.toLowerCase();
+                return resourceTitle.includes(filterWords) || resourceContent.includes(filterWords);
+            });
+            break;
+
+        case 'Author':
+            filteredContent = resources.filter(resource => {
+                const resourceAuthor = resource.author.toLowerCase();
+                return resourceAuthor.includes(filterWords);
+            });
+            break;
+
+        default:
+            break;
+        }
+    return filteredContent;
+}
+
+export function multipleFilterQuery(resources, option, conjunction, words) {
+    const filterWords = words.toLowerCase().split(/[ ,]+/).filter(Boolean);
+    let filteredContent = [];
+
+    if (conjunction === 'And') {
+        switch(option) {
+            case 'Title':
+                filteredContent = resources.filter(resource => {
+                    const resourceTitle = resource.title.toLowerCase();
+                    return filterWords.every(word => resourceTitle.includes(word))});
+                break;
+                
+            case 'Contents':
+                filteredContent = resources.filter(resource => {
+                    const resourceContent = resource.description.toLowerCase();
+                    return filterWords.every(word => resourceContent.includes(word))});
+                break;
+
+            case 'Title + Contents':
+                filteredContent = resources.filter(resource => {
+                    const resourceTitle = resource.title.toLowerCase();
+                    const resourceContent = resource.description.toLowerCase();
+                    return filterWords.every(word => resourceTitle.includes(word) || resourceContent.includes(word))});
+                break;
+                                        
+            default:
+                break;
+        }
+        
+    } else if (conjunction === 'Or') {
+        switch(option) {
+            case 'Title':
+                filteredContent = resources.filter(resource => {
+                    const resourceTitle = resource.title.toLowerCase();
+                    return filterWords.some(word => resourceTitle.includes(word))});
+                break;
+                
+            case 'Contents':
+                filteredContent = resources.filter(resource => {
+                    const resourceContent = resource.description.toLowerCase();
+                    return filterWords.some(word => resourceContent.includes(word))});
+                break;
+
+            case 'Title + Contents':
+                filteredContent = resources.filter(resource => {
+                    const resourceTitle = resource.title.toLowerCase();
+                    const resourceContent = resource.description.toLowerCase();
+                    return filterWords.some(word => resourceTitle.includes(word) || resourceContent.includes(word))});
+                break;
+
+            case 'Author':
+                filteredContent = resources.filter(resource => {
+                    const resourceAuthor = resource.author.toLowerCase();
+                    return filterWords.some(word => resourceAuthor.includes(word))});
+                break;
+                                        
+            default:
+                break;
+        }
+    }
+    return filteredContent;
+}
+
+export function sortQuery(type, resources, option) {
+    let sortedResources;
+    if (option === 'date') {
+        if (type === 'messages') {
+            sortedResources = resources.sort((a,b) => (a.updatedAt > b.updatedAt) ? -1 : ((b.updatedAt > a.updatedAt) ? 1 : 0));
+        } else {
+            sortedResources = resources.sort((a,b) => (a.createdAt > b.createdAt) ? -1 : ((b.createdAt > a.createdAt) ? 1 : 0))
+        }
+    } else if (option === 'views') {
+        sortedResources = resources.sort((a,b) => (a.views > b.views) ? -1 : ((b.views > a.views) ? 1 : 0))
+    } else {
+        if (type === 'messages') {
+            sortedResources = resources.sort((a,b) => (a.createdAt > b.createdAt) ? 1 : ((b.createdAt > a.createdAt) ? -1 : 0));
+        } else {
+            sortedResources = resources.sort((a,b) => (a.id > b.id) ? -1 : ((b.id > a.id) ? 1 : 0))
+        }
+    }
+    return sortedResources;
+}
