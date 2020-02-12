4317fff8b624c36c80761d488aa732a3d781e90a
Monica Lee <monicalee.rb@gmail.com>
2020-02-12 15:44:26 -0800

    Add event listeners to open dialog and/or redirect for search results.

--- src/Search.js
+++ src/Search.js
@@ -3,11 +3,13 @@ import { format } from 'date-fns';
 import { Container, Grid, makeStyles, Typography } from '@material-ui/core';
 import { AccountCircleOutlined, LocalOfferOutlined, ScheduleOutlined } from '@material-ui/icons';
 import withRoot from './withRoot';
-import NavBar from './views/NavBar';
+import { AuthUserContext } from './components/session';
+import ArticleDialog from './components/ArticleDialog';
 import SearchBar from './components/SearchBar';
 import Pagination from './components/Pagination';
-import Footer from './views/Footer';
 import { singleFilterQuery, multipleFilterQuery } from './constants/helpers';
+import NavBar from './views/NavBar';
+import Footer from './views/Footer';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -49,15 +51,30 @@ function Search(props) {
     searchOption: 'Title + Contents',
     searchConjunction: 'Or',
     searchQuery: '',
+    dialogOpen: false,
+    selectedResource: null,
     currentPage: 0,
     resourcesPerPage: 10
   }
   const classes = useStyles();
   const [ state, setState ] = useState(INITIAL_STATE);
-  const { searchedResources, searchCategory, searchOption, searchConjunction, currentPage, resourcesPerPage } = state;
-  
-  const handleSearchQuery = event => setState({...state, [event.target.name]: event.target.value});
+  const { searchedResources, searchCategory, searchOption, searchConjunction, dialogOpen, selectedResource, currentPage, resourcesPerPage } = state;
 
+  const handleResourceClick = selectedResource => {
+    if (Object.keys(selectedResource).includes('author')) {
+      setState({...state, dialogOpen: true, selectedResource });
+    } else {
+      props.history.push({
+        pathname: `/schools/${selectedResource.title.replace(/[^A-Z0-9]+/ig, "_").toLowerCase()}`,
+        state: {
+          title: 'School Information',
+          selected: 0,
+          selectedSchool: selectedResource
+        }
+      })}
+  }
+  const handleDialogClose = () => setState({...state, dialogOpen: false, selectedResource: null});
+  const handleSearchQuery = event => setState({...state, [event.target.name]: event.target.value});
   const handlePageChange = (event, newPage) => setState({...state, currentPage: newPage});
 
   const totalResources = searchedResources.length;
@@ -98,21 +115,24 @@ function Search(props) {
                     // alt=''
                 />
                 </Grid>
-                <Grid item xs={9} md={9} className={classes.body}>
+                <Grid item xs={9} md={9} className={classes.body} onClick={() => handleResourceClick(resource)}>
                   <div>{resource.title}</div>
                   <Grid container spacing={2} >
+                    {(resource.author) && 
                     <Grid item>
-                      <AccountCircleOutlined fontSize="small"/>
+                      <AccountCircleOutlined fontSize="small"/> 
                       {resource.author}
-                    </Grid>
+                    </Grid>}
+                    {(resource.tag) && 
                     <Grid item>
-                      <LocalOfferOutlined fontSize="small"/>
-                      {resource.tag}
-                    </Grid>
+                      <LocalOfferOutlined fontSize="small"/> 
+                      {resource.author}
+                    </Grid>}
+                    {(resource.updatedAt || resource.createdAt) && 
                     <Grid item>
-                    <ScheduleOutlined fontSize="small"/>
-                    {(resource && resource.updatedAt > resource.createdAt) ? format(resource.updatedAt, 'Pp') : format(resource.createdAt, 'Pp')}
-                    </Grid>
+                      <ScheduleOutlined fontSize="small"/> 
+                      {(resource && resource.updatedAt > resource.createdAt) ? format(resource.updatedAt, 'Pp') : format(resource.createdAt, 'Pp')}
+                    </Grid>}
                   </Grid>
                   <div>{resource.description}</div>
                 </Grid>
@@ -120,13 +140,18 @@ function Search(props) {
             </Fragment>
           )
         })}
-      <Typography variant="caption">{(searchCategory !== "All") && `More ${searchCategory.toUpperCase()}`}</Typography>
-      <Pagination 
-        totalPages={totalPages}
-        currentPage={currentPage} 
-        resourcesPerPage={resourcesPerPage}
-        handlePageChange={handlePageChange}
-      />
+      {/* <Typography variant="caption">{(searchCategory !== "All") && `More ${searchCategory.toUpperCase()}`}</Typography> */}
+      <AuthUserContext.Consumer>
+        { authUser => authUser &&
+          <ArticleDialog 
+          authUser={authUser} 
+          history={props.history}
+          articleOpen={dialogOpen} 
+          onClose={handleDialogClose} 
+          article={selectedResource}/>
+        }
+      </AuthUserContext.Consumer>
+      <Pagination totalPages={totalPages} currentPage={currentPage} resourcesPerPage={resourcesPerPage} handlePageChange={handlePageChange}/>
       </Container>
       <Footer />
     </>
