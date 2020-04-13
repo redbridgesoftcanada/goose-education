6c884326c670a134a8f1f7483d1fc742b78c8804
Monica Lee <lee.smonica@gmail.com>
2020-04-13 16:48:38 -0700

    Update layout of admin Goose Tips COMPOSE form to more of a preview feature.

--- src/components/admin/GooseTips.js
+++ src/components/admin/GooseTips.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Button, Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
-import { CheckBox, CheckBoxOutlineBlank, Add, Delete, Edit, Launch } from "@material-ui/icons";
+import { IconButton, Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
+import { CheckBox, CheckBoxOutlineBlank, Clear, Edit, PageviewOutlined } from "@material-ui/icons";
 import { format } from "date-fns";
 import { withFirebase } from "../../components/firebase";
 import AdminComposeDialog from './AdminComposeDialog';
@@ -9,26 +9,14 @@ import DeleteConfirmation from '../DeleteConfirmation';
 function GooseTips(props) {
   const { state, dispatch, listOfTips, firebase, history } = props;
 
+  // S T A T E
   const [ selectedTip, setSelectedTip ] = useState(null);
 
   // D I S P A T C H  M E T H O D S
   const setSnackbarMessage = message => dispatch({type: 'SNACKBAR_OPEN', payload: message});
-  const toggleComposeDialog = () => dispatch({type: 'TOGGLE_COMPOSE_DIALOG'});
+  const togglePreviewDialog = () => dispatch({type: 'TOGGLE_PREVIEW_DIALOG'});
   const toggleEditDialog = () => dispatch({type: 'TOGGLE_EDIT_DIALOG'});
   const toggleDeleteConfirm = () => dispatch({type: 'DELETE_CONFIRM'});
-  
-  const handleRedirect = id => {
-    setSelectedTip(listOfTips.find(tip => tip.id === id));
-    const redirectPath = {
-      pathname: '/goose', 
-      state: {
-        title: 'Goose Study Abroad',
-        selected: 1,
-        // selectedTip: selectedTip
-      }
-    }
-    history.push(redirectPath);
-  }
 
   const setEditTip = id => {
     setSelectedTip(listOfTips.find(school => school.id === id));
@@ -51,39 +39,47 @@ function GooseTips(props) {
 
   return (
     <>
-      {/* C R E A T E */}
-      <Button size="small" variant="contained" color="secondary" startIcon={<Add/>} onClick={toggleComposeDialog}>Create</Button>
-      <AdminComposeDialog formType="tip" isEdit={false} open={state.composeDialogOpen} onClose={toggleComposeDialog} setSnackbarMessage={setSnackbarMessage}/>
-
       {/* E D I T */}
-      <AdminComposeDialog formType="tip" isEdit={true} open={state.editDialogOpen} onClose={toggleEditDialog} setSnackbarMessage={setSnackbarMessage} prevContent={selectedTip}/>
+      <AdminComposeDialog 
+        formType="tip" 
+        isEdit={true} 
+        open={state.editDialogOpen} 
+        onClose={toggleEditDialog} 
+        setSnackbarMessage={setSnackbarMessage} 
+        prevContent={selectedTip}/>
 
       {/* D E L E T E */}
-      <DeleteConfirmation deleteType='admin_tip' open={state.deleteConfirmOpen} 
-      handleDelete={deleteTip} 
-      onClose={toggleDeleteConfirm}/>
+      <DeleteConfirmation 
+        deleteType='admin_tip' 
+        open={state.deleteConfirmOpen} 
+        handleDelete={deleteTip} 
+        onClose={toggleDeleteConfirm}/>
 
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>No</TableCell>
             <TableCell>Title</TableCell>
             <TableCell>Featured</TableCell>
             <TableCell>Last Updated At</TableCell>
-            <TableCell>Actions</TableCell>
+            <TableCell>Edit</TableCell>
+            <TableCell>Delete</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {listOfTips.map((tip, i) => (
             <TableRow key={i}>
-              <TableCell>{i + 1}</TableCell>
               <TableCell>{tip.title}</TableCell>
               <TableCell>{tip.isFeatured ? <CheckBox/> : <CheckBoxOutlineBlank/>}</TableCell>
               <TableCell>{format(tip.updatedAt, "Pp")}</TableCell>
               <TableCell>
-                <Button size="small" variant="contained" color="secondary" startIcon={<Launch/>} onClick={() => handleRedirect(tip.id)}>View</Button>
-                <Button size="small" variant="contained" color="secondary" startIcon={<Edit/>} onClick={() => setEditTip(tip.id)}>Edit</Button>
-                <Button size="small" variant="contained" color="secondary" startIcon={<Delete/>} onClick={() => setDeleteTip(tip.id)}>Delete</Button>
+                <IconButton color="secondary" onClick={() => setEditTip(tip.id)}>
+                  <Edit/>
+                </IconButton>
+              </TableCell>
+              <TableCell>
+                <IconButton color="secondary" onClick={() => setDeleteTip(tip.id)}>
+                  <Clear/>
+                </IconButton>
               </TableCell>
             </TableRow>
           ))}
--- src/components/admin/forms/TipsComposeForm.js
+++ src/components/admin/forms/TipsComposeForm.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useReducer } from "react";
-import { Button, CircularProgress, FormControlLabel, FormHelperText, FormLabel, RadioGroup, Radio, makeStyles } from "@material-ui/core";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-import { EditorValidator, FileValidator } from "../../../constants/customValidators";
+import { Button, CircularProgress, DialogContent, FormControlLabel, FormHelperText, FormLabel, RadioGroup, Radio, Typography, makeStyles } from "@material-ui/core";
+import { ValidatorForm } from "react-material-ui-form-validator";
 import { withFirebase } from "../../firebase";
+import * as COMPONENTS from "../../../constants/helpers-admin";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,6 +18,16 @@ const useStyles = makeStyles(theme => ({
   resetContainer: {
     padding: theme.spacing(3),
   },
+  image: {
+    display: 'block',
+    border: '0',
+    width: '100%',
+    maxWidth: '100%',
+    height: 'auto',
+  },
+  mt2: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 function toggleReducer(state, action) {
@@ -29,8 +39,8 @@ function toggleReducer(state, action) {
       return {...initialState}
 
     case "EDIT_STATE":
-      const { image, ...prepopulateForm } = payload;
-      return {...prepopulateForm, isEdit: true, isLoading: false, image: ""}
+      const prepopulateForm = payload;
+      return {...prepopulateForm, isEdit: true, isLoading: false}
     
     case "INIT_SAVE":
       return {...state, isLoading: true}
@@ -60,8 +70,10 @@ function toggleReducer(state, action) {
 }
 
 function TipsComposeForm(props) {
+  const classes = useStyles();
   const { dialogOpen, onDialogClose, setSnackbarMessage, firebase } = props;
 
+  // S T A T E
   const INITIAL_STATE = {
     isEdit: false,
     isLoading: false,
@@ -71,8 +83,17 @@ function TipsComposeForm(props) {
     description: ""
   }
   const [ state, dispatch ] = useReducer(toggleReducer, INITIAL_STATE);
-  const classes = useStyles();
 
+  useEffect(() => {
+    const prevContent = props.prevContent;
+    if (prevContent) {
+      dispatch({type:"EDIT_STATE", payload: prevContent});
+    } else {
+      dispatch({type:"RESET_STATE", payload: INITIAL_STATE});
+    }
+  }, [dialogOpen]);
+
+  // D I S P A T C H  M E T H O D S
   const handleTextInput = event => dispatch({type: "TEXT_INPUT", payload: event.target});
   const handleRichTextInput = (name, value) => dispatch({type: "RICH_TEXT_INPUT", payload: {name, value}});
   const handleFileUpload = event => dispatch({type: "FILE_UPLOAD", payload: event.target.files[0]});
@@ -110,58 +131,49 @@ function TipsComposeForm(props) {
     });
   }
 
-  useEffect(() => {
-    const prevContent = props.prevContent;
-    if (prevContent) {
-      dispatch({type:"EDIT_STATE", payload: prevContent});
-    } else {
-      dispatch({type:"RESET_STATE", payload: INITIAL_STATE});
-    }
-  }, [dialogOpen]);
-
   return (
     <ValidatorForm onSubmit={onSubmit}>
-      <div>
-        <FormLabel component="legend">Title</FormLabel>
-        <TextValidator 
-          type="text" 
-          variant="outlined" 
-          fullWidth 
-          InputLabelProps={{shrink: true}}
-          name="title"
-          value={state.title}
-          onChange={handleTextInput}
-          validators={["required"]}
-          errorMessages={[`Cannot submit an empty title.`]}
-        />
+      <FormLabel>Title</FormLabel>
+      {COMPONENTS.textValidator("title", state.title, handleTextInput)}
 
-        <FormLabel component="legend">Description</FormLabel>
-        <EditorValidator
-          defaultValue={state.description}
-          value={state.description} 
-          onChange={value => handleRichTextInput("description", value)}
-          validators={["isQuillEmpty"]}
-          errorMessages={["Cannot submit an empty post."]}/>
-
-        <FormLabel component="legend">Image</FormLabel>
-        <FileValidator
-          disableUnderline
-          onChange={handleFileUpload}
-          name="file"
-          value={state.image}
-          validators={["isRequiredUpload"]}
-          errorMessages={["Please upload an image."]} />
-
-        <FormHelperText>Display this tip on the home page.</FormHelperText>
-        <RadioGroup name="isFeatured" defaultValue={state.isFeatured} value={state.isFeatured} onChange={handleTextInput}>
-          <FormControlLabel value={true} control={<Radio/>} label="Yes" />
-          <FormControlLabel value={false} control={<Radio/>} label="No" />
-        </RadioGroup>
+      {(state.image) &&
+        <>
+          <FormLabel>Current Image</FormLabel>
+          <img className={classes.image} src={(state.image.includes('firebase')) ? state.image : require(`../../../assets/img/${state.image}`)} alt="cover image"/>
+        </>
+      }
+        
+      <div className={classes.mt2}>
+        <FormLabel>Upload Image</FormLabel>
+        <div>
+          {COMPONENTS.fileValidator("file", state.image, handleFileUpload)}
+        </div>
       </div>
 
-      <Button type="submit" color="secondary" autoFocus className={classes.button}>
-        {state.isLoading ? <CircularProgress /> : "Save"}
-      </Button>
+      <div className={classes.mt2}>
+        <FormLabel>Description</FormLabel>
+        {COMPONENTS.richTextValidator("description", state.description, handleRichTextInput)}
+      </div>
+      {/* </DialogContent> */}
+
+      <div className={classes.mt2}>
+      <FormLabel>Featured Tip</FormLabel>
+      <FormHelperText>This will display this tip on the Goose Home Page.</FormHelperText>
+      <RadioGroup name="isFeatured" defaultValue={state.isFeatured} value={state.isFeatured} onChange={handleTextInput}>
+        <FormControlLabel value={true} control={<Radio/>} label="Yes" />
+        <FormControlLabel value={false} control={<Radio/>} label="No" />
+      </RadioGroup>
+      </div>
+
+      {/* <div> */}
+        <Button className={classes.button} onClick={onDialogClose}>
+          Cancel
+        </Button>
+
+        <Button type="submit" color="secondary" autoFocus className={classes.button}>
+          {state.isLoading ? <CircularProgress /> : "Save"}
+        </Button>
+      {/* </div> */}
 
     </ValidatorForm>
   );
