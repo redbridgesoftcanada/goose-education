a8953b4b5b2fcf54f0d232752a599a1287c9c9ad
Monica Lee <monicalee.rb@gmail.com>
2020-05-28 14:37:29 -0700

    Create cloud function to increment totals into new aggregates collection/messages doc.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,48 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+const FieldValue = admin.firestore.FieldValue;
+
+// H E L P E R  F U N C T I O N S
+const aggregateTransaction = (ref, action) => {
+  return db.runTransaction(transaction => {
+    return transaction.get(ref).then(doc => {
+      if (!doc.exists) {
+        console.log("Document does not exist!");
+      }
+      transaction.update(ref, {total: FieldValue.increment(action)});
+      return true;
+    });
+  })
+  .then(() => { 
+    console.log(`Update (${action}) transaction successfully committed!`);
+    return true;
+  })
+  .catch(error => { 
+    console.log(`Update (${action}) transaction failed: `, error);
+    return;
+  });
+}
+
+// C L O U D  F U N C T I O N S
+exports.aggregateTotalMessages = functions.firestore
+  .document('messages/{messageId}')
+  .onWrite((change, context) => {
+    const aggregateMessageRef = db.collection('aggregates').doc('messages');
+    if (!change.before.exists) {
+        // New document Created : add one to total
+        aggregateTransaction(aggregateMessageRef, 1);
+        
+    } else if (!change.after.exists) {
+        // Deleting document : subtract one from total
+        aggregateTransaction(aggregateMessageRef, -1)
+
+    } else if (change.before.exists && change.after.exists) {
+        // Updating existing document : Do nothing
+        return;
+    }
+  return;
+});
