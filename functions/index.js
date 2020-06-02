ef5ba871f2dc4308ee35a871d3362e0b100ea338
Monica Lee <monicalee.rb@gmail.com>
2020-06-02 12:05:07 -0700

    Create aggregate cloud functions for applications (totals and statuses).

--- functions/index.js
+++ functions/index.js
@@ -6,43 +6,103 @@ admin.initializeApp();
 const db = admin.firestore();
 const FieldValue = admin.firestore.FieldValue;
 
+// C L O U D  F U N C T I O N S
+
+// !change.before.exists = no existing document in collection before update = new document created + add one to total field;
+// !change.after.exists = no existing document in collection after update = document deleted + subtract one from total field;
+// change.before.exists && change.after.exists = existing document in collection = do nothing;
+
+exports.aggregateMessages = functions.firestore
+  .document('messages/{messageId}')
+  .onWrite((change, context) => {
+    const aggregateMessageRef = db.collection('aggregates').doc('messages');
+    if (!change.before.exists) {
+      aggregateTotalsTransaction(aggregateMessageRef, 1);
+        
+    } else if (!change.after.exists) {
+      aggregateTotalsTransaction(aggregateMessageRef, -1)
+
+    } else if (change.before.exists && change.after.exists) {
+      return;
+    }
+  return;
+});
+
+exports.aggregateAirportRideApplications = functions.firestore
+  .document('airportRideApplications/{airportRideApplicationId}')
+  .onWrite((change, context) => {
+    const aggregateAirportRideApplicationsRef = db.collection('aggregates').doc('airportRideApplications');
+    if (!change.before.exists) {
+      aggregateTotalsTransaction(aggregateAirportRideApplicationsRef, 1);
+        
+    } else if (!change.after.exists) {
+      aggregateTotalsTransaction(aggregateAirportRideApplicationsRef, -1)
+
+    } else if (change.before.exists && change.after.exists) {
+      return;
+    }
+  return;
+});
+
+exports.aggregateHomestayApplications = functions.firestore
+  .document('homestayApplications/{homestayApplicationId}')
+  .onWrite((change, context) => {
+    const aggregateHomestayApplicationsRef = db.collection('aggregates').doc('homestayApplications');
+    if (!change.before.exists) {
+      aggregateTotalsTransaction(aggregateHomestayApplicationsRef, 1);
+        
+    } else if (!change.after.exists) {
+      aggregateTotalsTransaction(aggregateHomestayApplicationsRef, -1)
+
+    } else if (change.before.exists && change.after.exists) {
+      return;
+    }
+  return;
+});
+
+exports.aggregateSchoolApplications = functions.firestore
+  .document('schoolApplications/{schoolApplicationId}')
+  .onWrite((change, context) => {
+    const applicationStatus = change.after.exists ? change.after.data().status : null;
+
+    const aggregateSchoolApplicationsRef = db.collection('aggregates').doc('schoolApplications');
+    if (!change.before.exists) {
+      aggregateTotalsTransaction(aggregateSchoolApplicationsRef, 1, applicationStatus);
+
+    } else if (!change.after.exists) {
+      aggregateTotalsTransaction(aggregateSchoolApplicationsRef, -1, applicationStatus)
+
+    } else if (change.before.exists && change.after.exists) {
+      return;
+    }
+  return;
+});
+
 // H E L P E R  F U N C T I O N S
-const aggregateTransaction = (ref, action) => {
+const aggregateTotalsTransaction = (ref, increment, field) => {
   return db.runTransaction(transaction => {
     return transaction.get(ref).then(doc => {
       if (!doc.exists) {
         console.log("Document does not exist!");
       }
-      transaction.update(ref, {total: FieldValue.increment(action)});
+
+      if (field) {
+        transaction.update(ref, {
+          [field]: FieldValue.increment(increment), 
+          total: FieldValue.increment(increment)
+        });
+      } else {
+        transaction.update(ref, {total: FieldValue.increment(increment)});
+      }
       return true;
     });
   })
   .then(() => { 
-    console.log(`Update (${action}) transaction successfully committed!`);
+    console.log(`Update (${increment}) transaction successfully committed!`);
     return true;
   })
   .catch(error => { 
-    console.log(`Update (${action}) transaction failed: `, error);
+    console.log(`Update (${increment}) transaction failed: `, error);
     return;
   });
 }
-
-// C L O U D  F U N C T I O N S
-exports.aggregateTotalMessages = functions.firestore
-  .document('messages/{messageId}')
-  .onWrite((change, context) => {
-    const aggregateMessageRef = db.collection('aggregates').doc('messages');
-    if (!change.before.exists) {
-        // New document Created : add one to total
-        aggregateTransaction(aggregateMessageRef, 1);
-        
-    } else if (!change.after.exists) {
-        // Deleting document : subtract one from total
-        aggregateTransaction(aggregateMessageRef, -1)
-
-    } else if (change.before.exists && change.after.exists) {
-        // Updating existing document : Do nothing
-        return;
-    }
-  return;
-});
