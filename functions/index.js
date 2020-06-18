566699e470a4edd6f28167b9b1892253a3baf3f5
Monica Lee <lee.smonica@gmail.com>
2020-06-18 10:52:54 -0700

    > Create 'generatePDF' function to create, upload, and access download URL for schoolApplication documents; > Refactor aggregations to use one configure helper function (debug infinite loop triggers from change.before/change.after);

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const PDFDocument = require('pdfkit');
+const serviceAccount = require('./serviceAccount.json');
 
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
@@ -16,7 +17,7 @@ exports.aggregateTips = functions.firestore
   .document('tips/{tipId}')
   .onWrite((change, context) => {
     const tipRef = db.collection('aggregates').doc('tips');
-    const aggregateTips = (!change.after.exists) ? aggregateTotalsTransaction(tipRef, -1) : aggregateTotalsTransaction(tipRef, 1);
+    const aggregateTips = configureAggregateHelpers(tipRef, 1, '', change);
     return aggregateTips;
 });
 
@@ -24,7 +25,7 @@ exports.aggregateMessages = functions.firestore
   .document('messages/{messageId}')
   .onWrite((change, context) => {
     const messageRef = db.collection('aggregates').doc('messages');
-    const aggregateMessages = (!change.after.exists) ? aggregateTotalsTransaction(messageRef, -1) : aggregateTotalsTransaction(messageRef, 1);
+    const aggregateMessages = configureAggregateHelpers(messageRef, 1, '', change);
     return aggregateMessages;
 });
 
@@ -32,7 +33,7 @@ exports.aggregateAirportRideApplications = functions.firestore
   .document('airportRideApplications/{airportRideApplicationId}')
   .onWrite((change, context) => {
     const otherApplicationsRef = db.collection('aggregates').doc('otherApplications');
-    const aggregateOtherApplications = (!change.after.exists) ? aggregateTotalsTransaction(otherApplicationsRef, -1, 'airportRidesTotal') : aggregateTotalsTransaction(otherApplicationsRef, 1, 'airportRidesTotal');
+    const aggregateOtherApplications = configureAggregateHelpers(otherApplicationsRef, 1, 'airportRidesTotal', change);
     return aggregateOtherApplications;
 });
 
@@ -40,35 +41,36 @@ exports.aggregateHomestayApplications = functions.firestore
   .document('homestayApplications/{homestayApplicationId}')
   .onWrite((change, context) => {
     const otherApplicationsRef = db.collection('aggregates').doc('otherApplications');
-    const aggregateOtherApplications = (!change.after.exists) ? aggregateTotalsTransaction(otherApplicationsRef, -1, 'homestayTotal') : aggregateTotalsTransaction(otherApplicationsRef, 1, 'homestayTotal');
+    const aggregateOtherApplications = configureAggregateHelpers(otherApplicationsRef, 1, 'homestayTotal', change);
     return aggregateOtherApplications;
 });
 
 exports.aggregateSchoolApplications = functions.firestore
   .document('schoolApplications/{schoolApplicationId}')
-  .onWrite((change, context) => {
-    // invocation (1): aggregate total number of applications by school;
-    const schoolsRef = db.collection('aggregates').doc('schools');
-    const school = change.after.get('schoolName');
-    const aggregateSchools = (!change.after.exists) ? aggregateTotalsTransaction(schoolsRef, -1, school) : aggregateTotalsTransaction(schoolsRef, 1, school);
+  .onWrite(async (change, context) => {
+    // A G G R E G A T E (total number of applications by school);
+    // const schoolsRef = db.collection('aggregates').doc('schools');
+    // const school = change.after.get('schoolName');
+    // const aggregateSchools = (!change.after.exists) ? await aggregateTotalsTransaction(schoolsRef, -1, school) : await aggregateTotalsTransaction(schoolsRef, 1, school);
 
-    // invocation (2): aggregate total number of applications by application status;
-    const applicationsRef = db.collection('aggregates').doc('schoolApplications');
-    const aggregateApplications = configureAggregateHelpers(applicationsRef, 1, "status", change);
+    // A G G R E G A T E (total number of applications by status); 
+    // const applicationsRef = db.collection('aggregates').doc('schoolApplications');
+    // const aggregateApplications = await configureAggregateHelpers(applicationsRef, 1, 'status', change);
 
-    // invocation (3): create downloadable PDF of school application;
-    const applicationId = context.params.schoolApplicationId;
+    // O T H E R (create downloadable PDF of school application);
     const newApplicationData = change.after.data();
-    const generatePDFApplication = generatePDF(applicationId, newApplicationData);
+    const applicationId = context.params.schoolApplicationId;
+    const generatePDFApplication = await generatePDF(applicationId, newApplicationData, change);
 
-    return aggregateApplications && aggregateSchools && generatePDFApplication;
+    Promise.all([generatePDFApplication])
+    .catch(err => console.log('Caught error in schoolApplications parent trigger ', err));
 });
 
 exports.aggregateArticles = functions.firestore
   .document('articles/{articleId}')
   .onWrite((change, context) => {
     const articleRef = db.collection('aggregates').doc('articles');
-    const aggregateArticles = configureAggregateHelpers(articleRef, 1, "tag", change);
+    const aggregateArticles = configureAggregateHelpers(articleRef, 1, 'tag', change);
     return aggregateArticles;
 });
 
@@ -76,24 +78,32 @@ exports.aggregateAnnounces = functions.firestore
   .document('announcements/{announceId}')
   .onWrite((change, context) => {
     const announceRef = db.collection('aggregates').doc('announcements');
-    const aggregateAnnounces = configureAggregateHelpers(announceRef, 1, "tag", change);
+    const aggregateAnnounces = configureAggregateHelpers(announceRef, 1, 'tag', change);
     return aggregateAnnounces;
 });
 
 // H E L P E R  F U N C T I O N S
 const configureAggregateHelpers = (ref, increment, field, change) => {
-  const newValue = change.after.get(field);
-  const oldValue = change.before.get(field);
-
-  if (newValue === oldValue) return null;
-
+  
   let aggregateValues;
-  if (!change.before.exists) {
-    aggregateValues = aggregateTotalsTransaction(ref, increment, newValue);
-  } else if (!change.after.exists) {
-    aggregateValues = aggregateMultiTotalsTransaction(ref, -increment, oldValue, newValue);
+  
+  if (field) {
+    const checkNestedTotals = field === 'airportRidesTotal' || field === 'homestayTotal';
+    const newValue = change.after.get(field);
+    const oldValue = change.before.get(field);
+
+    if (checkNestedTotals) {
+      aggregateValues = (!change.after.exists) ? aggregateTotalsTransaction(ref, -increment, field) : aggregateTotalsTransaction(ref, increment, field);
+    } else if (!change.before.exists) {
+      aggregateValues = aggregateTotalsTransaction(ref, increment, newValue);
+    } else if (!change.after.exists) {
+      aggregateValues = aggregateMultiTotalsTransaction(ref, -increment, oldValue, newValue);
+    } else {
+      aggregateValues = aggregateMultiTotalsTransaction(ref, increment, oldValue, newValue);
+    }
+
   } else {
-    aggregateValues = aggregateMultiTotalsTransaction(ref, increment, oldValue, newValue);
+    aggregateValues = (!change.after.exists) ? aggregateTotalsTransaction(ref, -increment) : aggregateTotalsTransaction(ref, increment);
   }
 
   return aggregateValues;
@@ -104,7 +114,7 @@ const aggregateTotalsTransaction = (ref, increment, field) => {
     return transaction.get(ref)
     .then(doc => {
       if (!doc.exists) {
-        console.log("Document does not exist!");
+        console.log('Document does not exist!');
         return;
       }
       if (field) {
@@ -133,7 +143,7 @@ const aggregateMultiTotalsTransaction = (ref, increment, prevField, newField) =>
     return transaction.get(ref)
     .then(doc => {
       if (!doc.exists) {
-        console.log("Document does not exist!");
+        console.log('Document does not exist!');
         return;
       }
 
@@ -162,22 +172,51 @@ const aggregateMultiTotalsTransaction = (ref, increment, prevField, newField) =>
   });
 }
 
-function generatePDF(id, form) {
-  const doc = new PDFDocument;                        // create an instance = READABLE Node stream;
-  const filename = `${id}/test-${Date.now()}.pdf`;
-  const file = bucket.file(filename);                 // BUCKET METHOD: creates a File object from Bucket (methods: createWriteStream, getSignedUrl);
-  const bucketFileStream = file.createWriteStream();  // FILE METHOD: creates a writeable stream to overwrite the contents of the file in your bucket;
+async function generatePDF(id, form, change) {
+  let schoolApplicationRef = db.collection('schoolApplications').doc(`${id}`);
+  let filename = `applications/${form.schoolName}-${id}.pdf`;
+  let file = bucket.file(filename);                         // BUCKET METHOD: creates a File object from Bucket (methods: createWriteStream, getSignedUrl);
 
-  doc.pipe(bucketFileStream);                         // send the output of the PDF document to another WRITABLE Node stream;
+  const checkIfFileExists = await file.exists();
   
-  doc.end();                                          // finalize the PDF file;
+  if (checkIfFileExists[0]) {
+    // console.log(`File already exists in Storage for ${id}.`);
+    
+    if (!form.downloadUrl) {
+      // console.log(`Missing download URL in Firebase for ${id}.`);
+      return generateDownloadUrl(schoolApplicationRef, form, file);
+    }
+    // console.log("Nothing to do, file and download URL exists.")
+    return;
+  }
 
-  bucketFileStream.on("finish", function () {
-    console.log('the file upload is completed');
+  const doc = new PDFDocument;                              // create an instance = READABLE Node stream;
+  const bucketFileStream = file.createWriteStream();        // FILE METHOD: creates a writeable stream to overwrite the contents of the file in your bucket;
+
+  doc.pipe(bucketFileStream);                               // send the output of the PDF document to another WRITABLE Node stream;
+  
+  doc.end();                                                // finalize the PDF file;
+
+  bucketFileStream.on('finish', () => {
+    // console.log(`Finished writeable stream for ${id}.`);
+    return generateDownloadUrl(schoolApplicationRef, form, file);
   });
 
-  bucketFileStream.on("error", function(err) {
-    console.error(err);
+  bucketFileStream.on('error', e => {
+    console.error(e);
   });
+}
 
+function generateDownloadUrl(ref, form, file) {
+  try {
+    return file.getSignedUrl({action: 'read', expires: '03-17-2025'}).then(signedUrl => {
+      if (signedUrl[0] === form.downloadUrl) {
+        console.log('I have nothing to do!');
+        return;
+      }
+      return ref.set({ downloadUrl: signedUrl[0] }, { merge: true });
+    });
+  } catch(e) {
+    console.log(`Caught error in access and set download URL to Firebase for ${form.authorID}: `, e)
+  }
 }
