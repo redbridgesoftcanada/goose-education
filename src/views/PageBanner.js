bbebe92b1fe470d96a250a32c7559f8bbd0a29b1
Monica Lee <lee.smonica@gmail.com>
2020-07-13 12:47:01 -0700

    Refactor conditionals for page banner styling with tenaries (prev. switch statements) for DRY code.

--- src/views/PageBanner.js
+++ src/views/PageBanner.js
@@ -1,59 +1,38 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import { Link as RouterLink } from "react-router-dom";
-
 import Button from '../components/onePirate/Button';
 import Typography from '../components/onePirate/Typography';
 import PageBannerLayout from './PageBannerLayout';
 
 const useStyles = (props, options) => {
-  let styles;
-  switch(props.layoutType){
-    case 'headerBanner':
-      styles = makeStyles(theme => ({
-        background: {
-          backgroundImage: `url(${props.backgroundImage})`,
-          backgroundPosition: 'center',
+  return makeStyles(theme => ({
+    background: {
+      backgroundImage: `url(${props.backgroundImage})`,
+      backgroundPosition: 'center',
+    },
+    title: {
+      position: 'absolute',
+      color: theme.palette.common.white,
+      ...(props.layoutType === 'headerBanner') ? {marginTop: 20} : {marginTop: 90},
+    },
+    ...(props.layoutType === 'headerBanner') && {
+      button: {
+        minWidth: 200,
+      },
+      h5: {
+        marginBottom: theme.spacing(4),
+        marginTop: theme.spacing(4),
+        [theme.breakpoints.up('sm')]: {
+          marginTop: theme.spacing(10),
         },
-        title: {
-          position: 'absolute',
-          marginTop: 20,
-          color: theme.palette.common.white,
-        },
-      }))(props, options);
-      break;
-
-    case 'pageBanner':
-      styles = makeStyles(theme => ({
-        background: {
-          backgroundImage: `url(${props.backgroundImage})`,
-          backgroundPosition: 'center',
-        },
-        title: {
-          position: 'absolute',
-          marginTop: 90,
-          color: theme.palette.common.white,
-        },
-        button: {
-          minWidth: 200,
-        },
-        h5: {
-          marginBottom: theme.spacing(4),
-          marginTop: theme.spacing(4),
-          [theme.breakpoints.up('sm')]: {
-            marginTop: theme.spacing(10),
-          },
-        },
-        more: {
-          marginTop: theme.spacing(2),
-          marginBottom: theme.spacing(2),
-        },
-      }))(props, options); 
-      break;
-
-      default:
-  }
-  return styles;
+      },
+      more: {
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+      }
+    }
+  }))(props, options);
 }
 
 function PageBanner(props) {
@@ -66,39 +45,38 @@ function PageBanner(props) {
         {/* Increase the network loading priority of the background image. */}
         <img style={{display:'none'}} src={backgroundImage} alt='header background banner'/>
         {(layoutType === 'pageBanner') ?
-          <>
-            <Typography color="inherit" align="left" variant="h2" marked="center">
-              {title}
-            </Typography>
-            <Typography  color="inherit" align="left" variant="body1" className={classes.more}>
-              {props.caption}
-            </Typography>
-            <Button
-              color="secondary"
-              variant="contained"
-              size="medium"
-              className={classes.button}
-              component={RouterLink}
-              to=
-              {{
-                pathname: '/goose', 
-                state: {
-                  title: 'Goose Study Abroad',
-                  selected: 0
-                }
-              }}
-            >
-              View More
-            </Button>
-          </>
-        :  
-          <Typography className={classes.title} align="center" variant="h4">
-            {title}
-          </Typography>
+          loadPageBanner(classes, title, props.caption)
+          :  
+          <Typography className={classes.title} align="center" variant="h4">{title}</Typography>
         }
       </PageBannerLayout>
     </>
   );
+}
+
+function loadPageBanner(classes, title, caption) {
+  return (
+    <>
+      <Typography color="inherit" align="left" variant="h2" marked="center">{title}</Typography>
+      <Typography  color="inherit" align="left" variant="body1" className={classes.more}>{caption}</Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="medium"
+        className={classes.button}
+        component={RouterLink}
+        to={{
+          pathname: '/goose', 
+          state: {
+            title: 'Goose Study Abroad',
+            selected: 0
+          }
+        }}
+      >
+        View More
+      </Button>
+    </>
+  )
 }
 
 export default PageBanner;
