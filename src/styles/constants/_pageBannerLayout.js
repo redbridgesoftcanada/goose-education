a814f979b81a0ca5f86a8047bc353f3c19204eae
Monica Lee <monicalee.rb@gmail.com>
2020-07-30 13:25:32 -0700

    Replace conditional styling for clsx class management library for pageBannerLayout component.

--- src/styles/constants/_pageBannerLayout.js
+++ src/styles/constants/_pageBannerLayout.js
@@ -1,10 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const pageBannerLayoutStyles = props => {
-  const isPageBanner = props.layoutType === 'pageBanner';
-  const isHeaderBanner = props.layoutType === 'headerBanner';
-  const isPoster = !isPageBanner && !isHeaderBanner;
-  
+    
   const flexContainerStyles = { display: 'flex', flexDirection: 'column', alignItems: 'center' };
   const backgroundStyles = { position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }; 
   
@@ -12,45 +9,75 @@ export const pageBannerLayoutStyles = props => {
     root: {
       alignItems: 'center',
       color: theme.palette.common.white,
-      height: isPoster ? '45vh' : isHeaderBanner ? '22vh' : '80vh',
-      minHeight: !isPageBanner ? 205 : 500,
       maxHeight: 1300,
       position: 'relative',
-      ...!isPageBanner && { display: 'flex' },
-      ...isPageBanner && {
-        [theme.breakpoints.down('sm')]: { 
-          height: '40vh'
-        }
+    },
+    
+    pageRoot: {
+      height: '80vh',
+      minHeight: 500,
+      [theme.breakpoints.down('sm')]: { 
+        height: '40vh'
       }
     },
 
-    container: {
-      marginBottom: isPoster ? theme.spacing(20) : theme.spacing(7),
-      marginTop: !isPageBanner ? 0 : '21vh',
+    headerRoot: {
+      height: '22vh',
+      minHeight: 205,
+      display: 'flex',
+    },
 
-      ...!isPageBanner && { 
+    posterRoot: {
+      height: '45vh',
+      minHeight: 205,
+      display: 'flex',
+    },
+
+    customRoot: {
+      height: '50vh',
+      minHeight: 205,
+      display: 'flex',
+    },
+
+    defaultContainer: {
+      ...flexContainerStyles, 
+      marginTop: 0,
+      marginBottom: theme.spacing(7),
+      maxWidth: 'inherit',
+    },
+
+    pageContainer: {
+      marginBottom: theme.spacing(7),
+      marginTop: '21vh',
+      float: 'left',
+      marginLeft: 40, 
+      width: 815,
+      [theme.breakpoints.down('md')]: {
         ...flexContainerStyles, 
-        maxWidth: 'inherit',
+        marginLeft: 70, 
+        width: '85%'
+      }, 
+      [theme.breakpoints.down('sm')]: {
+        marginLeft: 0, 
+        width: 'auto'
       },
-      
-      ...isPageBanner &&  { 
-        float: 'left',
-        marginLeft: 40, 
-        width: 815,
-        [theme.breakpoints.down('md')]: {
-          ...flexContainerStyles, 
-          marginLeft: 70, 
-          width: '85%'
-        }, 
-        [theme.breakpoints.down('sm')]: {
-          marginLeft: 0, 
-          width: 'auto'
-        },
-        [theme.breakpoints.down('xs')]: {
-          marginTop: '15vh', 
-          width: 'auto'
-        }
+      [theme.breakpoints.down('xs')]: {
+        marginTop: '15vh', 
+        width: 'auto'
       }
+    },
+
+    posterContainer: {
+      ...flexContainerStyles, 
+      marginTop: 0,
+      marginBottom: theme.spacing(20),
+      maxWidth: 'inherit',
+    },
+
+    customContainer: {
+      ...flexContainerStyles, 
+      marginTop: 0,
+      maxWidth: 'inherit',
     },
 
     backdrop: {
--- src/views/PageBannerLayout.js
+++ src/views/PageBannerLayout.js
@@ -6,10 +6,30 @@ import useStyles from '../styles/constants';
 function PageBannerLayout(props) {
   const classes = useStyles(props, 'pageBannerLayout');
   const { backgroundClassName, children } = props;
+
+  const isPageBanner = props.layoutType === 'pageBanner';
+  const isHeaderBanner = props.layoutType === 'headerBanner';
+  const isCustomLayout = props.layoutType === 'vancouver_now';
+  const isPoster = !isPageBanner && !isHeaderBanner && !isCustomLayout
+
+  const rootStyle = clsx({
+    [classes.root]: true,
+    [classes.pageRoot]: isPageBanner,
+    [classes.headerRoot]: isHeaderBanner,
+    [classes.customRoot]: isCustomLayout,
+    [classes.posterRoot]: isPoster
+  });
+
+  const containerStyle = clsx({
+    [classes.defaultContainer]: !isPageBanner,
+    [classes.pageContainer]: isPageBanner,
+    [classes.posterContainer]: isPoster,
+    [classes.customContainer]: isCustomLayout
+  })
   
   return (
-    <section className={classes.root}>
-      <Container className={classes.container}>
+    <section className={rootStyle}>
+      <Container className={containerStyle}>
         {children}
         <div className={classes.backdrop} />
         <div className={clsx(classes.background, backgroundClassName)} />
