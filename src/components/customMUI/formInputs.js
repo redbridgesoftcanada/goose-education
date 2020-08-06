4022326e58759673902c81847f73abcf048f7e62
Monica Lee <lee.smonica@gmail.com>
2020-08-06 15:23:08 -0700

    *WIP. Refactor form inputs as neutral, functional components, rather than functions (validation rules passed as props, if needed).

--- src/components/customMUI/formInputs.js
+++ src/components/customMUI/formInputs.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { TextValidator } from "react-material-ui-form-validator";
-import { OutlinedInput, FormControlLabel, FormHelperText, MenuItem, Radio, RadioGroup, Select, Checkbox } from "@material-ui/core";
-import { EditorValidator, FileValidator, SelectValidator } from "./validators";
+import { FormLabel, OutlinedInput, FormControlLabel, FormHelperText, MenuItem, Radio, RadioGroup, Select, Checkbox } from "@material-ui/core";
+import { QuillValidator, FileValidator, SelectValidator, RadioGroupValidator, DatePickerValidator, DateTimePickerValidator } from "./validators";
 import { convertToSentenceCase } from '../../constants/helpers/_features';
+import useStyles from '../../styles/constants';
+
+const classes = useStyles({}, 'validations');
 
 // N O  V A L I D A T I O N
 function textField(name, value, eventHandler, isMultiline) {
@@ -32,25 +35,6 @@ function defaultValueTextField(name, value, eventHandler, isMultiline) {
   )
 }
 
-function radioField(name, value, options, eventHandler, helperText) {
-  // note. options needs to be an array of objects: {value, label}
-  
-  if (name === "type") {  // options = [SCHOOL_TYPES] (string) from SchoolsComposeForm;
-    options = options.map(type => ({value:type, label:type}));
-  }
-
-  return (
-    <>
-      {helperText && <FormHelperText>{helperText}</FormHelperText>}
-      <RadioGroup name={name} value={value} onChange={eventHandler}>
-        {options.map((option, i) => (
-          <FormControlLabel key={i} value={option.value} control={<Radio/>} label={option.label} />
-        ))}
-      </RadioGroup>
-    </>
-  )
-}
-
 function selectField(name, value, options, eventHandler) {
   // note. options needs to be an array (not of objects);
 
@@ -68,16 +52,37 @@ function selectField(name, value, options, eventHandler) {
     </Select>
   )
 }
+function customRadioGroup(name, value, options, onChangeHandler, label, helperText) {
+  // note. options needs to be an array of objects: {value, label}
+  
+  if (name === "type") {  
+    // options = [SCHOOL_TYPES] (string) from SchoolsComposeForm;
+    options = options.map(type => ({value:type, label:type}));
+  }
 
-function checkboxField(state, name, label, text, eventHandler) {
+  return (
+    <>
+      <FormLabel component="legend" className={classes.legend}>{label}</FormLabel>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+      <RadioGroup name={name} value={value} onChange={onChangeHandler}>
+        {options.map((option, i) => (
+          <FormControlLabel key={i} value={option.value} control={<Radio/>} label={option.label} />
+        ))}
+      </RadioGroup>
+    </>
+  )
+}
+
+function customCheckboxField(isChecked, name, label, text, onChangeHandler) {
   return (
     <>
     <FormControlLabel
       control={
         <Checkbox 
-          checked={state} 
+          size="small"
+          checked={isChecked} 
           name={name} 
-          onChange={eventHandler}
+          onChange={onChangeHandler}
         />
       }
       label={label}
@@ -88,20 +93,6 @@ function checkboxField(state, name, label, text, eventHandler) {
 }
 
 // V A L I D A T I O N
-function customTextValidator(name, value, handleChange, props) {
-  return (
-    <TextValidator 
-      variant="outlined" 
-      fullWidth 
-      InputLabelProps={{shrink: true}}
-      name={name}
-      value={value}
-      onChange={handleChange}
-      {...props}
-    />
-  )
-}
-
 function firebaseValidator(type, name, value, placeholder, eventHandler, error) {
   const checkError = error => {
     if (error.code.includes("email") || error.code.includes("argument")) {
@@ -122,7 +113,7 @@ function firebaseValidator(type, name, value, placeholder, eventHandler, error) 
 }
 
 function textValidator(name, value, handleValue, props) {
-  const errorField = convertToSentenceCase(name).toLowerCase();
+  const errorField = convertToSentenceCase(name);
   return (
     <TextValidator 
       fullWidth 
@@ -131,16 +122,30 @@ function textValidator(name, value, handleValue, props) {
       name={name}
       value={value}
       onChange={handleValue}
-      validators={["required"]}
-      errorMessages={[`Cannot submit an empty ${errorField}.`]}
+      validators={["required", "isEmpty"]}
+      errorMessages={[`${errorField} is required`, `Cannot submit an empty ${errorField.toLowerCase()}`]}
       {...props}
+    />
+  )
+}
+
+function customTextField(name, value, handleChange, customProps) {
+  return (
+    <TextValidator 
+      variant="outlined" 
+      fullWidth 
+      InputLabelProps={{shrink: true}}
+      name={name}
+      value={value}
+      onChange={handleChange}
+      {...customProps}
     />
   )
 }
 
 function richTextValidator(name, value, eventHandler) {
   return (
-    <EditorValidator
+    <QuillValidator
       defaultValue={value}
       onChange={value => eventHandler(name, value)}
       validators={["isQuillEmpty"]}
@@ -170,7 +175,7 @@ function selectValidator(name, value, options, eventHandler) {
       value={value}
       onChange={eventHandler}
       validators={['required']}
-      errorMessages={['Please select a tag.']}>
+      errorMessages={['Please select an option.']}>
         <MenuItem value="" disabled>Select One</MenuItem>
         {options.map((option, i) => {
             return <MenuItem key={i} name={option} value={option}>{option}</MenuItem>
@@ -179,4 +184,140 @@ function selectValidator(name, value, options, eventHandler) {
   )
 }
 
-export { textField, radioField, selectField, checkboxField, customTextValidator, firebaseValidator, textValidator, richTextValidator, fileValidator, selectValidator, defaultValueTextField }
+function radioGroupValidator(name, value, options, eventHandler) {
+  return (
+    <RadioGroupValidator 
+      name={name} 
+      value={value} 
+      onChange={eventHandler}
+      validators={['required']}
+      errorMessages={['Please select an option.']}>
+      {options.map((option, i) => (
+        <FormControlLabel 
+          key={i} 
+          value={option.value} 
+          control={<Radio/>} 
+          label={option.label}/>
+      ))}
+    </RadioGroupValidator>
+  )
+}
+
+function customDatePicker(name, value, onChangeHandler, label, customProps) {
+  return (
+    <>
+      <FormLabel component="legend" className={classes.legend}>{label}</FormLabel>
+      <DatePickerValidator
+        variant="inline"
+        format="MM/dd/yyyy"
+        name={name}
+        value={value}
+        onChange={onChangeHandler}
+        {...customProps}
+        />
+    </>
+  )
+}
+
+function customDateTimePicker(name, value, onChangeHandler, label, customProps) {
+  return (
+    <>
+      <FormLabel component="legend" className={classes.legend}>{label}</FormLabel>
+      <DateTimePickerValidator
+      variant="inline"
+      format="MM/dd/yyyy HH:mm"
+      name={name}
+      value={value}
+      onChange={onChangeHandler}
+      disablePast={true}
+      {...customProps}
+      />
+    </>
+  )
+}
+
+
+// ================================================================
+
+function TextField(props) {
+  const { label, ...customProps } = props;
+  return (
+    <>
+      <FormLabel component="legend" className={classes.legend}>{label}</FormLabel>
+      <TextValidator 
+        fullWidth 
+        variant="outlined" 
+        InputLabelProps={{shrink: true}}
+        {...customProps}
+      />
+    </>
+  )
+}
+
+function CustomSelect(props) {
+  const { options, ...customProps } = props;
+  return (
+    <SelectValidator
+      variant="outlined"
+      displayEmpty
+      {...customProps}>
+        <MenuItem value="" disabled>Select One</MenuItem>
+        {options.map((option, i) => 
+          <MenuItem 
+            key={i} 
+            name={option} 
+            value={option}>
+              {option}
+          </MenuItem>
+        )}
+    </SelectValidator>
+  )
+}
+
+function CustomRadioGroup(props) {
+  const { label, options, ...customProps } = props;
+  return (
+    <>
+      <FormLabel component="legend" className={classes.legend}>{label}</FormLabel>
+      <RadioGroupValidator {...customProps}>
+        {options.map((option, i) => 
+          <FormControlLabel 
+            key={i} 
+            control={<Radio/>} 
+            value={option.value} 
+            label={option.label}/>
+        )}
+      </RadioGroupValidator>
+    </>
+  )
+}
+
+function CustomCheckbox(props) {
+  const { label, ...customProps } = props;
+  return (
+    <FormControlLabel
+      label={label}
+      control={
+        <Checkbox size="small" {...customProps}/>}
+    />
+  )
+      // {text && <FormHelperText>{text}</FormHelperText>}
+}
+
+function CustomDatePicker(props) {
+  const { label, ...customProps } = props;
+  return (
+    <>
+      <FormLabel component="legend" className={classes.legend}>{label}</FormLabel>
+      <DatePickerValidator
+        variant="inline"
+        format="MM/dd/yyyy"
+        {...customProps}
+        />
+    </>
+  )
+}
+
+export { textField, customRadioGroup, selectField, customCheckboxField, customTextField, firebaseValidator, textValidator, richTextValidator, fileValidator, selectValidator, defaultValueTextField, radioGroupValidator, customDatePicker, customDateTimePicker,
+
+TextField, CustomSelect, CustomRadioGroup, CustomCheckbox, CustomDatePicker }
--- src/views/SchoolApplication.js
+++ src/views/SchoolApplication.js
@@ -1,39 +1,60 @@
-import React, { Fragment, useReducer } from 'react';
+import React, { Fragment, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { Button, Container, FormLabel, Typography } from '@material-ui/core';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 import { ValidatorForm } from "react-material-ui-form-validator";
 import { format } from 'date-fns';
-import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
-import { withAuthorization } from '../components/session';
 import { STATUSES, PERSONAL_FIELDS, PROGRAM_FIELDS, ARRIVAL_FIELDS, OTHER_FIELDS } from '../constants/constants';
 import { convertToCamelCase, convertToTitleCase } from '../constants/helpers/_features';
-import { textField, checkboxField, textValidator, selectValidator, radioValidator } from '../components/customMUI/formInputs';
+import { withAuthorization } from '../components/session';
+import ErrorSnackbar from '../components/ErrorSnackbar';
+import * as StyledValidator from '../components/customMUI/formInputs';
+
+// import { textField, customCheckboxField, textValidator, selectValidator, radioGroupValidator } from '../components/customMUI/formInputs';
 import { useStyles } from '../styles/schools';
 
-let INITIAL_STATE = {
-  isLoading: false,
-  isError: false,
-  agreeToPrivacy: false
-}
+let INITIAL_STATE = { agreeToPrivacy: false }
 
-function SchoolApplicationBase(props) {
+// configure INITIAL_STATE values for all form fields;
+const formFields = [ PERSONAL_FIELDS, OTHER_FIELDS[1], PROGRAM_FIELDS, OTHER_FIELDS[0], ARRIVAL_FIELDS[0], OTHER_FIELDS[2] ].flat();
+configureFormDefaults(formFields);
+
+function SchoolApplication(props) {
   const classes = useStyles(props, 'schoolApplication');
   const history = useHistory();
   const { authUser, firebase, listOfSchoolNames } = props;
 
-  const formFields = [ PERSONAL_FIELDS, OTHER_FIELDS[1], PROGRAM_FIELDS, OTHER_FIELDS[0], ARRIVAL_FIELDS[0], OTHER_FIELDS[2] ].flat();
-  configureFormFields(formFields);
+  const [ userInput, setUserInput ] = useState(INITIAL_STATE);
+  const [ progress, setProgress ] = useState({ loading: false, error: false, message: null });
 
-  const [ state, dispatch ] = useReducer(toggleReducer, INITIAL_STATE);
+  console.log(userInput)
 
-  // E V E N T  L I S T E N E R S
-  const handleFormInput = event => dispatch({type: event.target});
+  const handleFormInput = e => {
+    const field = e.currentTarget.name;
+    const input = e.currentTarget.value;
+    setUserInput(prevState => ({...prevState, [field]: input}));
+  }
 
-  const handleCheckboxInput = event => dispatch({ type: 'checkbox', payload: event.target });
+  const handleCheckboxInput = e => {
+    const field = e.currentTarget.name;
+    const checked = e.currentTarget.checked;
+    setUserInput(prevState => ({...prevState, [field]: checked}));
+  }
+
+  const handlePickerInput = (date, field) => {
+    setUserInput(prevState => ({...prevState, [field]: date}));
+  }
 
   const onSubmit = event => {
-    const { isLoading, isError, agreeToPrivacy, ...schoolApplication } = state;
+    setProgress(prevState => ({...prevState, loading: true}));
+
+    const { agreeToPrivacy, ...schoolApplication } = userInput;
+
+    if (!agreeToPrivacy) {
+      setProgress({loading: false, error: true, message: 'Please review and agree to the privacy agreement.'});
+      return;
+    }
 
     firebase.schoolApplications(authUser.uid).add({
       authorID: authUser.uid,
@@ -41,17 +62,43 @@ function SchoolApplicationBase(props) {
       updatedAt: Date.now(),
       status: STATUSES[0],
       ...schoolApplication})
-    .then(() => history.push('/profile'))
-    // .catch(error => dispatch({type: 'error', payload: error}));
+    .then(() => {
+      setProgress(prevState => ({...prevState, loading: false}));
+      history.push('/profile');
+    })
+    .catch(err => 
+      setProgress({loading: false, error: true, message: err}));
+
     event.preventDefault();
   }
 
   return (
     <Container>
+
+      {progress.error && 
+        <ErrorSnackbar 
+          isOpen={progress.error}
+          onCloseHandler={() => setProgress({loading: false, error: false, message: null})}
+          errorMessage={progress.message}/>
+      }
+
       <ValidatorForm className={classes.root} onSubmit={onSubmit}>
         {formFields.map(field => {
-          const titleField = convertToTitleCase(field);
+          const formLabel = convertToTitleCase(field);
           field = convertToCamelCase(field);
+
+          const inputProps = {
+            key: field,
+            name: field,
+            value: userInput[field],
+            label: formLabel,
+            onChange: handleFormInput
+          }
+
+          const validationRules = {
+            validators: ['required', 'isQuillEmpty'],
+            errorMessages: ['required', '']
+          }
 
           switch(field) {
             case 'gender': {              
@@ -61,23 +108,22 @@ function SchoolApplicationBase(props) {
                 { value: "other", label: "Other" }
               ];
               
-              return (
-                <Fragment key={field}>
-                  <FormLabel component="legend" className={classes.legend}>{titleField}</FormLabel>
-                  {radioValidator(field, state[field], options, handleFormInput, "")}
-                </Fragment>
-              )}
+              return <StyledValidator.CustomRadioGroup 
+                {...inputProps} 
+                {...validationRules}
+                options={options}/>
+            }
             
             case 'birthDate':
             case 'programStartDate':
             case 'arrivalFlightDate':
               return (
                 <MuiPickersUtilsProvider utils={DateFnsUtils} key={field}>
-                  <FormLabel component="legend" className={classes.legend}>{titleField}</FormLabel>
+                  <FormLabel component="legend" className={classes.legend}>{formLabel}</FormLabel>
                   <KeyboardDatePicker
                   name={field}
-                  value={state[field]}
-                  onChange={date => dispatch({type: field, payload: date})}
+                  value={userInput[field]}
+                  onChange={date => handlePickerInput(date, field)}
                   variant="inline"
                   format="MM/dd/yyyy"
                   {...(field !== "birthDate") && { disablePast: true }}
@@ -90,66 +136,67 @@ function SchoolApplicationBase(props) {
                 { value: "yes", label: "Yes" }, 
                 { value: "no", label: "No" }
               ];
-              return (
-                <Fragment key={field}>
-                  <FormLabel component="legend" className={classes.legend}>{titleField}</FormLabel>
-                  {radioValidator(field, state[field], options, handleFormInput, "")}
-                </Fragment>
-              )}
-              
-              case 'schoolName':
-                return (
-                  <Fragment key={field}>
-                    <FormLabel component="legend" className={classes.legend}>{titleField}</FormLabel>
-                    {selectValidator(field, state[field], listOfSchoolNames, handleFormInput)}
-                  </Fragment>
-                );
 
-              case 'additionalRequests': 
-                return (
-                  <Fragment key={field}>
-                    <Typography variant="h6">{titleField}</Typography>
-                    {textField(field, state[field], handleFormInput, true)}
-                  </Fragment>
-                );
+              return <StyledValidator.CustomRadioGroup {...inputProps} options={options}/>
+            }
+              
+            case 'schoolName':
+              return <StyledValidator.CustomSelect {...inputProps} options={listOfSchoolNames}/>
+
+            case 'additionalRequests': 
+              return (
+                <StyledValidator.TextField 
+                  {...inputProps}
+                  multiline={true}
+                  rows={5}/>
+              );
 
             default:
               return (
-              <Fragment key={field}>
-                <FormLabel component="legend" className={classes.legend}>{titleField}</FormLabel>    
-                {textValidator(field, state[field], handleFormInput)}
-              </Fragment>
-              )
-            }
+                <StyledValidator.TextField 
+                  {...inputProps} 
+                  {...validationRules}
+                />
+            )}
           })
         }
 
         <Typography variant="h6">Privacy</Typography>
         <Typography align="left" variant="body2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
         
-        {checkboxField(state.agreeToPrivacy, 'agreeToPrivacy', <Typography variant="body2">I agree to the Privacy Agreement.</Typography>, '', handleCheckboxInput)}
+        <StyledValidator.CustomCheckbox 
+          checked={userInput.agreeToPrivacy}
+          name='agreeToPrivacy'
+          onChange={handleCheckboxInput}
+          label={<Typography variant="body2">I agree to the Privacy Agreement.</Typography>}
+        />
 
         <Button variant="contained" color="secondary" type="submit">Submit Application</Button>
       </ValidatorForm>
     </Container>
 )}
 
-function configureFormFields(formFields) {
+function configureFormDefaults(formFields) {
   formFields.map(field => {
     field = convertToCamelCase(field);
-    return INITIAL_STATE = field.includes('Date') ? {...INITIAL_STATE, [field]: format(Date.now(), 'MM/dd/yyyy')} : {...INITIAL_STATE, [field]: ''}
+    if (field.includes('Date')) {
+      INITIAL_STATE = { ...INITIAL_STATE, [field]: format(Date.now(), 'MM/dd/yyyy') }
+    } else {
+      INITIAL_STATE = {...INITIAL_STATE, [field]: ''}
+    }
+    return INITIAL_STATE;
   });
 }
 
-function toggleReducer(state, action) {
-  const { type, payload } = action;
-  const { name, value } = type;
+// function toggleReducer(state, action) {
+//   const { type, payload } = action;
+//   const { name, value } = type;
 
-  if (type === 'checkbox') return { ...state, [payload.name]: payload.checked }
-  if (typeof type === 'string' && type.includes('Date')) return { ...state, [type]: payload }
-  // if (type === 'submit') return { ...INITIAL_STATE }
-  return { ...state, [name]: value }
-}
+//   if (type === 'checkbox') return { ...state, [payload.name]: payload.checked }
+//   if (typeof type === 'string' && type.includes('Date')) return { ...state, [type]: payload }
+//   // if (type === 'submit') return { ...INITIAL_STATE }
+//   return { ...state, [name]: value }
+// }
 
 const condition = authUser => !!authUser;
-export default withAuthorization(condition)(SchoolApplicationBase);
+export default withAuthorization(condition)(SchoolApplication);
