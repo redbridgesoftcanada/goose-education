1eb5ec98f39466610b8fb1e01ef7a7b8e4597faf
Monica Lee <monicalee.rb@gmail.com>
2020-08-27 12:31:04 -0700

    > Convert from inline to impoted stylesheet; > Debug issue with 'listOfGraphics' props passed as an obj rather than arr ( = Settings page crash); > Pass new 'listOfImages' prop for new tab section on Settings page.

--- src/components/admin/TableTemplate.js
+++ src/components/admin/TableTemplate.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import { CircularProgress, Link, Typography, makeStyles } from "@material-ui/core";
+import { CircularProgress, Link, Typography } from "@material-ui/core";
 import Title from "./Title";
 import { DatabaseContext } from '../../components/database';
 import Accounts from '../admin/Accounts';
@@ -12,12 +12,116 @@ import Articles from '../admin/Articles';
 import Announcements from '../admin/Announcements';
 import Messages from '../admin/Messages';
 import Settings from '../admin/Settings';
+import { useStyles } from '../../styles/adminDashboard';
 
-const useStyles = makeStyles(theme => ({
-  seeMore: {
-    marginTop: theme.spacing(3),
-  },
-}));
+export default function TableTemplate(props) {
+  const classes = useStyles();
+
+  const INITIAL_STATE = {
+    isLoading: false,
+    anchorUserRole: null,
+    anchorApplicationStatus: null,
+    deleteConfirmOpen: false,
+    snackbarOpen: false,
+    snackbarMessage: null,
+    composeDialogOpen: false,
+    editDialogOpen: false,
+  }
+  const [ state, dispatch ] = useReducer(toggleReducer, INITIAL_STATE);
+
+  return (
+    <>
+      <Title>{props.type}</Title>
+      <DatabaseContext.Consumer>
+        {context => 
+          <>
+            {generateContentTable(state, dispatch, props, context)}
+            {props.type !== "Settings" && generatePagination(classes, state, dispatch, props.type, context)}
+          </>
+        }
+      </DatabaseContext.Consumer>
+    </>
+  );
+}
+
+function generateContentTable(state, dispatch, props, context) {
+  const { type, snackbarMessage } = props;
+  const customProps = { state, dispatch };
+
+  switch(type) {
+    case "Users":
+      customProps.listOfUsers = context.state.listOfUsers;
+      return <Accounts {...customProps}/>
+    
+    case "Schools":
+      customProps.listOfSchools = context.state.listOfSchools;
+    return <Schools {...customProps}/>
+
+    case "Applications":
+      customProps.listOfApplications = context.state.listOfApplications;
+      return <Applications {...customProps}/>
+    
+    case "Homestays":
+      customProps.listOfHomestays = context.state.listOfHomestays;
+      return <Homestays {...customProps}/>
+
+    case "Airport Rides":
+      customProps.listOfAirportRides = context.state.listOfAirportRides;
+      return <AirportRides {...customProps}/>
+
+    case "Goose Tips":
+      customProps.listOfTips = context.state.gooseTips;
+      return <GooseTips {...customProps}/>
+
+    case "Articles":
+      customProps.listOfArticles = context.state.listOfArticles;
+      return <Articles {...customProps}/>
+    
+    case "Announcements":
+      customProps.listOfAnnouncements = context.state.listOfAnnouncements;
+      return <Announcements {...customProps}/>
+
+    case "Messages":
+      customProps.listOfMessages = context.state.listOfMessages;
+      return <Messages {...customProps}/>
+
+    case "Settings":
+      customProps.snackbarMessage = snackbarMessage;
+      customProps.listOfGraphics = Object.values(context.state.adminGraphics).sort((a, b) => {
+        if (a.location < b.location) return -1;
+        if (a.location > b.location) return 1;
+        return 0;
+      });
+      customProps.listOfImages = Object.values(context.state.adminGraphics).flatMap(graphic => graphic.image ? [{id: graphic.id, url: graphic.image}] : []);
+      return <Settings {...customProps}/>
+
+    default:
+      return <Typography>Sorry! No corresponding content to display.</Typography>
+  }
+}
+
+function generatePagination(classes, state, dispatch, type, context) {
+  const loadMoreHandler = type => {
+    dispatch({type:"TOGGLE_LOADING"});
+    context.paginatedQuery(type);
+    dispatch({type:"TOGGLE_LOADING"});
+  }
+
+  return (
+    <div className={classes.seeMore}>
+      {state.isLoading ? 
+      <CircularProgress color="secondary"/>
+      :
+      context.state.isQueryEmpty[type] ? 
+      <Typography>All {type} Data Loaded.</Typography>
+      :
+      <Link color="secondary" href="#" onClick={() => loadMoreHandler(type)}>
+        Load More
+      </Link>
+      }
+    </div>
+  )
+}
 
 function toggleReducer(state, action) {
   const { type, payload } = action;
@@ -70,114 +174,3 @@ function toggleReducer(state, action) {
     
   }
 }
-
-function generateContentTable(state, dispatch, props, context) {
-  const { type, snackbarMessage } = props;
-  const customProps = { state, dispatch };
-
-  switch(type) {
-    case "Users":
-      customProps.listOfUsers = context.state.listOfUsers;
-      return <Accounts {...customProps}/>
-    
-    case "Schools":
-      customProps.listOfSchools = context.state.listOfSchools;
-    return <Schools {...customProps}/>
-
-    case "Applications":
-      customProps.listOfApplications = context.state.listOfApplications;
-      return <Applications {...customProps}/>
-    
-    case "Homestays":
-      customProps.listOfHomestays = context.state.listOfHomestays;
-      return <Homestays {...customProps}/>
-
-    case "Airport Rides":
-      customProps.listOfAirportRides = context.state.listOfAirportRides;
-      return <AirportRides {...customProps}/>
-
-    case "Goose Tips":
-      customProps.listOfTips = context.state.gooseTips;
-      return <GooseTips {...customProps}/>
-
-    case "Articles":
-      customProps.listOfArticles = context.state.listOfArticles;
-      return <Articles {...customProps}/>
-    
-    case "Announcements":
-      customProps.listOfAnnouncements = context.state.listOfAnnouncements;
-      return <Announcements {...customProps}/>
-
-    case "Messages":
-      customProps.listOfMessages = context.state.listOfMessages;
-      return <Messages {...customProps}/>
-
-    case "Settings":
-      customProps.snackbarMessage = snackbarMessage;
-      customProps.listOfGraphics = context.state.adminGraphics.sort((a, b) => {
-        if (a.location < b.location) return -1;
-        if (a.location > b.location) return 1;
-        return 0;
-      });
-      return <Settings {...customProps}/>
-
-    default:
-      return <Typography>Sorry! No corresponding content to display.</Typography>
-  }
-}
-
-function generatePagination(classes, state, dispatch, type, context) {
-  const loadMoreHandler = type => {
-    dispatch({type:"TOGGLE_LOADING"});
-    context.paginatedQuery(type);
-    dispatch({type:"TOGGLE_LOADING"});
-  }
-
-  return (
-    <div className={classes.seeMore}>
-      {state.isLoading ? 
-      <CircularProgress color="secondary"/>
-      :
-      context.state.isQueryEmpty[type] ? 
-      <Typography>All {type} Data Loaded.</Typography>
-      :
-      <Link color="secondary" href="#" onClick={() => loadMoreHandler(type)}>
-        Load More
-      </Link>
-      }
-    </div>
-  )
-}
-
-function TableTemplate(props) {
-  const classes = useStyles();
-
-  // S T A T E
-  const INITIAL_STATE = {
-    isLoading: false,
-    anchorUserRole: null,
-    anchorApplicationStatus: null,
-    deleteConfirmOpen: false,
-    snackbarOpen: false,
-    snackbarMessage: null,
-    composeDialogOpen: false,
-    editDialogOpen: false,
-  }
-  const [ state, dispatch ] = useReducer(toggleReducer, INITIAL_STATE);
-
-  return (
-    <>
-      <Title>{props.type}</Title>
-      <DatabaseContext.Consumer>
-        {context => 
-          <>
-            {generateContentTable(state, dispatch, props, context)}
-            {props.type !== "Settings" && generatePagination(classes, state, dispatch, props.type, context)}
-          </>
-        }
-      </DatabaseContext.Consumer>
-    </>
-  );
-}
-
-export default TableTemplate;
