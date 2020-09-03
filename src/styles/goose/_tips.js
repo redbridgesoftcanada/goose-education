2c99dcddea7bc73efa14706b825d7730c0afbfea
Monica Lee <monicalee.rb@gmail.com>
2020-09-03 10:36:50 -0700

    Add and remove unused style classes from GooseTips refactor.

--- src/styles/goose/_tips.js
+++ src/styles/goose/_tips.js
@@ -2,78 +2,38 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const tipsStyles = props => {
   return makeStyles(theme => ({
-    root: {
-      overflow: 'hidden',
-    },
-
-    image: {
-      display: 'block',
-      border: 0,
-      width: 'auto',
-      maxWidth: '100%',
-      height: 'auto',
-      margin: '0 auto',
-    },
-
-    item: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      padding: theme.spacing(3, 5),
-      textAlign: 'left',
-      "&:hover": {
-          cursor: 'pointer',
-      },
-    },
-
     title: {
       marginTop: theme.spacing(7),
       marginBottom: theme.spacing(2),
     },
-
-    body: {
-      display: 'flex',
-      flexDirection: 'column',
-      padding: theme.spacing(1, 0),
+    
+    board: {
+      marginTop: theme.spacing(7)
     },
 
-    articleTitle: {
-      ...theme.typography.subtitle2,
+    tipThumbnail: {
+      height: 200
     },
 
-    articleDescription: {
-      ...theme.typography.body2,
-      width: `${theme.spacing(5)}vh`,
-
-      [theme.breakpoints.between('sm', 'md')]: {
-        width: `${theme.spacing(4)}vh`
-      },
-      [theme.breakpoints.between('xs', 'sm')]: {
-        width: `${theme.spacing(6)}vh`
+    tip: {
+      color: theme.palette.common.black,
+      textDecoration: 'none',
+      '&:hover': {
+        color: theme.palette.secondary.main,
+        cursor: 'pointer',
+        textDecoration: 'none',
       }
     },
 
-    search: {
-      float: 'right',
-      border: `2px solid ${theme.palette.secondary.main}`,
-      borderRadius: 5,
-      paddingLeft: theme.spacing(1),
+    metaContainer: {
+      padding: theme.spacing(1, 0),
+      flexDirection: 'row'
     },
 
-    searchButton: {
-      color: theme.palette.common.white,
-      backgroundColor: theme.palette.secondary.main,
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(2)
+    tipImage: {
+      height: 300,
+      marginTop: theme.spacing(2)
     },
 
-    filterButton: {
-      float: 'left',
-      color: theme.palette.primary.light,
-      border: `2px solid ${theme.palette.primary.light}`,
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(2),
-      marginRight: theme.spacing(1),
-    }
   }))(props)
 }
