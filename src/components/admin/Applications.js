ebfa552adfd1e6fd5e1ee79c77491e92ddd4e8b8
Monica Lee <monicalee.rb@gmail.com>
2020-09-23 11:59:57 -0700

    Change applications (school, homestay, airport ride) deletion to remove both downloadable PDF from Storage + document from Firebase collection.

--- src/components/admin/Applications.js
+++ src/components/admin/Applications.js
@@ -9,10 +9,8 @@ import DeleteConfirmation from '../DeleteConfirmation';
 function Applications(props) {
   const { state, dispatch, listOfApplications, firebase } = props;
 
-  // S T A T E
   const [ selectedApplication, setSelectedApplication ] = useState(null);
 
-  // D I S P A T C H  M E T H O D S
   const setSnackbarMessage = message => dispatch({type: 'SNACKBAR_OPEN', payload: message});
   const toggleDeleteConfirm = () => dispatch({type: 'DELETE_CONFIRM'});
 
@@ -34,76 +32,80 @@ function Applications(props) {
     }
   }
 
-  const setDeleteApplication = id => {
-    setSelectedApplication(listOfApplications.find(application => application.authorID === id));
+  const setDeleteApplication = event => {
+    const findApplicationData = listOfApplications.find(application => application.id === event.currentTarget.id);
+    setSelectedApplication(findApplicationData);
     toggleDeleteConfirm();
   }
 
-  const deleteApplication = () => {
-    firebase.deleteSchoolApplication(selectedApplication.authorID).then(function() {
-     dispatch({type: 'DELETE_CONFIRM'});
-     setSnackbarMessage('Application successfully deleted!');
-    }).catch(function(error) {
-      console.log(error)
-    });
+  const deleteApplication = async () => {
+    const deleteStorageResource = firebase.refFromUrl(selectedApplication.downloadUrl).delete();
+    const deleteDoc =  firebase.deleteSchoolApplication(selectedApplication.id);
+
+    try {
+      await Promise.all([deleteStorageResource, deleteDoc]);
+      dispatch({type: 'DELETE_CONFIRM'});
+      setSnackbarMessage('Application successfully deleted!');
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 
   return (
     <>
-      {/* D E L E T E */}
       <DeleteConfirmation deleteType='admin_application' open={state.deleteConfirmOpen} 
       handleDelete={deleteApplication} 
       onClose={toggleDeleteConfirm}/>
 
       <Table size="small">
-      <TableHead>
-        <TableRow>
-          <TableCell>First Name</TableCell>
-          <TableCell>Last Name</TableCell>
-          <TableCell>School Name</TableCell>
-          <TableCell>Program</TableCell>
-          <TableCell>Start Date</TableCell>
-          <TableCell>Date Submitted</TableCell>
-          <TableCell>Status</TableCell>
-          <TableCell>Delete</TableCell>
-          <TableCell>Download</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {listOfApplications.map((application, i) => (
-          <TableRow key={i} hover>
-            <TableCell>{application.firstName}</TableCell>
-            <TableCell>{application.lastName}</TableCell>
-            <TableCell>{application.schoolName}</TableCell>
-            <TableCell>{application.programName}</TableCell>
-            <TableCell>{application.startDate}</TableCell>
-            <TableCell>{format(application.createdAt, "Pp")}</TableCell>
-            <TableCell>
-              <Button id={application.id} onClick={setMenuOpen}>{application.status}</Button>
-              <Menu
-                keepMounted
-                anchorEl={state.anchorApplicationStatus}
-                open={Boolean(state.anchorApplicationStatus)}
-                onClose={setMenuClose}
-              >
-                {STATUSES.map((status, i) => 
-                  <MenuItem key={i} id={status} onClick={setMenuClose}>{status}</MenuItem>
-                )}
-              </Menu>
-            </TableCell>
-            <TableCell>
-              <IconButton color="secondary" onClick={() => setDeleteApplication(application.authorID)}>
-                <Clear/>
-              </IconButton>
-            </TableCell>
-            <TableCell>
-              <IconButton color="secondary" onClick={() => window.open(application.downloadUrl)}>
-                <CloudDownload/>
-              </IconButton>
-            </TableCell>
+        <TableHead>
+          <TableRow>
+            <TableCell>First Name</TableCell>
+            <TableCell>Last Name</TableCell>
+            <TableCell>School Name</TableCell>
+            <TableCell>Program</TableCell>
+            <TableCell>Start Date</TableCell>
+            <TableCell>Date Submitted</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Delete</TableCell>
+            <TableCell>Download</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
+        </TableHead>
+        <TableBody>
+          {listOfApplications.map((application, i) => (
+            <TableRow key={i} hover>
+              <TableCell>{application.firstName}</TableCell>
+              <TableCell>{application.lastName}</TableCell>
+              <TableCell>{application.schoolName}</TableCell>
+              <TableCell>{application.programName}</TableCell>
+              <TableCell>{application.startDate}</TableCell>
+              <TableCell>{format(application.createdAt, "Pp")}</TableCell>
+              <TableCell>
+                <Button id={application.id} onClick={setMenuOpen}>{application.status}</Button>
+                <Menu
+                  keepMounted
+                  anchorEl={state.anchorApplicationStatus}
+                  open={Boolean(state.anchorApplicationStatus)}
+                  onClose={setMenuClose}
+                >
+                  {STATUSES.map((status, i) => 
+                    <MenuItem key={i} id={status} onClick={setMenuClose}>{status}</MenuItem>
+                  )}
+                </Menu>
+              </TableCell>
+              <TableCell>
+                <IconButton id={application.id} color="secondary" onClick={setDeleteApplication}>
+                  <Clear/>
+                </IconButton>
+              </TableCell>
+              <TableCell>
+                <IconButton color="secondary" onClick={() => window.open(application.downloadUrl)}>
+                  <CloudDownload/>
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
       </Table>
     </>
   )
--- src/components/admin/Homestays.js
+++ src/components/admin/Homestays.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IconButton, Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
 import { Clear, CloudDownload } from "@material-ui/icons";
+import { format, parseISO } from 'date-fns';
 import { withFirebase } from "../firebase";
 import DeleteConfirmation from '../DeleteConfirmation';
 
@@ -14,18 +15,22 @@ function Homestays(props) {
   const setSnackbarMessage = message => dispatch({type: 'SNACKBAR_OPEN', payload: message});
   const toggleDeleteConfirm = () => dispatch({type: 'DELETE_CONFIRM'});
 
-  const setDeleteApplication = id => {
-    setSelectedApplication(listOfHomestays.find(application => application.id === id));
+  const setDeleteApplication = event => {
+    const findHomestayData = listOfHomestays.find(application => application.id === event.currentTarget.id)
+    setSelectedApplication(findHomestayData);
     toggleDeleteConfirm();
   }
 
-  const deleteApplication = () => {
-    firebase.deleteHomestayApplication(selectedApplication.id).then(function() {
-     dispatch({type: 'DELETE_CONFIRM'});
-     setSnackbarMessage('Application successfully deleted!');
-    }).catch(function(error) {
-      console.log(error)
-    });
+  const deleteApplication = async () => {
+    const deleteStorageResource = firebase.refFromUrl(selectedApplication.downloadUrl).delete();
+    const deleteDoc =  firebase.deleteHomestayApplication(selectedApplication.id);
+    try {
+      await Promise.all([deleteStorageResource, deleteDoc]);
+      dispatch({type: 'DELETE_CONFIRM'});
+      setSnackbarMessage('Application successfully deleted!');
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 
   return (
@@ -42,30 +47,31 @@ function Homestays(props) {
           <TableCell>Last Name</TableCell>
           <TableCell>Start Date</TableCell>
           <TableCell>End Date</TableCell>
-          <TableCell>Arrival Flight</TableCell>
+          <TableCell>Arrival Date</TableCell>
           <TableCell>Delete</TableCell>
           <TableCell>Download</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
-        {listOfHomestays.map((application, i) => {
-          const formattedArrivalTime = Date(application.arrivalFlightTime).toString();
+        {listOfHomestays.map(application => {
           return (
-          <TableRow key={i} hover>
+          <TableRow key={application.id} hover>
             <TableCell>{application.firstName}</TableCell>
             <TableCell>{application.lastName}</TableCell>
-            <TableCell>{application.homestayStartDate}</TableCell>
-            <TableCell>{application.homestayEndDate}</TableCell>
-            <TableCell>{application.arrivalFlightDate} {formattedArrivalTime}</TableCell>
+            <TableCell>{format(application.homestayStartDate.toDate(), 'P')}</TableCell>
+            <TableCell>{format(application.homestayEndDate.toDate(), 'P')}</TableCell>
+            <TableCell>{format(application.arrivalFlightDate.toDate(), 'Pp')}</TableCell>
             <TableCell>
-              <IconButton color="secondary" onClick={() => setDeleteApplication(application.authorID)}>
+              <IconButton id={application.id} color="secondary" onClick={setDeleteApplication}>
                 <Clear/>
               </IconButton>
             </TableCell>
             <TableCell>
-              <IconButton color="secondary" onClick={()=>console.log('Clicked')}>
-                <CloudDownload/>
-              </IconButton>
+              {application.downloadUrl &&
+                <IconButton color="secondary" onClick={() => window.open(application.downloadUrl)}>
+                  <CloudDownload/>
+                </IconButton>
+              }
             </TableCell>
           </TableRow>
         )})}
