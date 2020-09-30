1417cfc7054664b14f9d577603e05b01303a0c62
Monica Lee <monicalee.rb@gmail.com>
2020-09-30 13:12:46 -0700

    Create random colour generation function (prev. hardcoded, limit 4) for admin chart content.

--- src/components/admin/ChartTemplate.js
+++ src/components/admin/ChartTemplate.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useTheme } from '@material-ui/core';
+import { red, pink, deepPurple, indigo, blue, cyan, teal, orange } from '@material-ui/core/colors';
 import { BarChart, Bar, Tooltip, XAxis, YAxis, Label, ResponsiveContainer, PieChart, Pie, Cell, Legend } from 'recharts';
 import Title from './Title';
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const muiHues = [red, pink, deepPurple, indigo, blue, cyan, teal, orange];
+const muiShades = [300, 400, 500, 600, 700, 800, 900];
+
+function createColourPalette(entries) {  
+  const palette = [];
+  do {
+    const rHue = muiHues[Math.floor(Math.random() * muiHues.length)];
+    const rShade = muiShades[Math.floor(Math.random() * muiShades.length)];
+    
+    if (!palette.includes(rHue[rShade])) palette.push(rHue[rShade]);
+  } 
+  while (palette.length <= entries);
+
+  return palette;
+}
 
 export default function Chart(props) {
   const theme = useTheme();
 
   const { chart, data } = props;
 
+  const COLORS = useRef(null);
+  if (!COLORS.current) COLORS.current = createColourPalette(data.plots.length);
+
   if (!data) return null;
 
   const renderPieLegend = dataPlots => {
     return dataPlots.map((entry, i) => (
-      { value: entry.Name, type: 'square', id:`item-${i}`, color: COLORS[i % COLORS.length] }));
+      { value: entry.Name, type: 'square', id:`item-${i}`, color: COLORS.current[i % COLORS.current.length] }));
   }
 
   return (
@@ -33,7 +51,7 @@ export default function Chart(props) {
             }}
           >
             <XAxis dataKey={data.xAxisKey} stroke={theme.palette.text.secondary} />
-            <YAxis stroke={theme.palette.text.secondary}>
+            <YAxis allowDecimals={false} stroke={theme.palette.text.secondary}>
               <Label
                 angle={270}
                 position="left"
@@ -49,14 +67,14 @@ export default function Chart(props) {
       }
 
       {chart === 'pie' &&
-      <ResponsiveContainer>
+      <ResponsiveContainer width={500} height="100%">
         <PieChart>
           <Pie
             data={data.plots}
             outerRadius={80}
             fill="#8884d8"
             dataKey={data.dataKey}>
-            {data.plots.map((entry, i) => <Cell key={`cell-${i}`} fill={COLORS[i % COLORS.length]} />)}
+            {data.plots.map((entry, i) => <Cell key={`cell-${i}`} fill={COLORS.current[i % COLORS.current.length]} />)}
           </Pie>
           <Legend layout="vertical" verticalAlign="middle" align="right" payload={renderPieLegend(data.plots)}/>
         </PieChart>
