611e80304b5e062cb4d57e986664ab01c7f456b5
Monica Lee <lee.smonica@gmail.com>
2020-10-02 11:39:24 -0700

    Refresh Instagram API access token before media request (= debug invalid/expiry within 60 days).

--- src/constants/helpers/_instagramAPI.js
+++ src/constants/helpers/_instagramAPI.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 export async function fetchUserMedia(setState) {
   try {
-    const requestUserMedia = await axios.get(`https://graph.instagram.com/me/media?fields=caption,media_url,permalink&access_token=${process.env.REACT_APP_INSTAGRAM_ACCESS_TOKEN}&limit=6`);
+    const refreshToken = await axios.get(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${process.env.REACT_APP_INSTAGRAM_ACCESS_TOKEN}`);
+    
+    const requestUserMedia = await axios.get(`https://graph.instagram.com/me/media?fields=caption,media_url,permalink&access_token=${refreshToken.data.access_token}&limit=6`);
+
     setState(prevState => ({ ...prevState, instagram: requestUserMedia.data.data }));
   } catch(e) {
     console.error(e);
