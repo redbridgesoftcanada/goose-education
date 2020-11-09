c110af9aab56226e5cce9d3002c5dc62b55f72f7
Monica Lee <lee.smonica@gmail.com>
2020-11-09 10:49:16 -0800

    Create debounce function to delay setting state for text inputs.

---
+++ src/constants/helpers/_debounce.js
@@ -0,0 +1,9 @@
+const debounce = (fn, delay) => {
+  let timeoutId;
+  return function(...args) {
+    clearInterval(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), delay);
+  }
+}
+
+export default debounce;
